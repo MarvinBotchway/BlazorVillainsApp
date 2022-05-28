da3fb5d02b0ee5b598f24dd48f69e9e109538ef6
Marvin Botchway <mnob.700@gmail.com>
2022-05-28 14:48:34 +0000

    Add Models To Shared Project
    Create Models folder and put ComicModel and VillainModel into the Models folder

---
+++ BlazorVillainsApp/Shared/Models/VillainModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorVillainsApp.Shared.Models
+{
+    public class VillainModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string VillainName { get; set; } = string.Empty;
+        public ComicModel Comic { get; set; } = new ComicModel();
+        public int ComicId { get; set; }
+    }
+}
