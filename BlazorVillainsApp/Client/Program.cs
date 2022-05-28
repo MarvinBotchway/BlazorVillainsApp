65bbb0aad353bd44cc0df5136be7b3170150a72e
Marvin Botchway <mnob.700@gmail.com>
2022-05-28 23:30:22 +0000

    Register Service
    Registered Service in Program.cs

--- BlazorVillainsApp/Client/Program.cs
+++ BlazorVillainsApp/Client/Program.cs
@@ -1,4 +1,5 @@
 global using BlazorVillainsApp.Shared.Models;
+global using BlazorVillainsApp.Client.Services.VillainServices;
 
 using BlazorVillainsApp.Client;
 using Microsoft.AspNetCore.Components.Web;
@@ -9,5 +10,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped<IVillainService, VillainService>();
 await builder.Build().RunAsync();
