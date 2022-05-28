0f9261eec68fe397defb2669172ef27edc9ab480
Marvin Botchway <mnob.700@gmail.com>
2022-05-28 23:07:57 +0000

    Add Services Folder And Add VillainsService
    Created Services and Interface, But implemented only the GetVillains() method.

---
+++ BlazorVillainsApp/Client/Services/VillainServices/IVillainService.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorVillainsApp.Client.Services.VillainServices
+{
+    public interface IVillainService
+    {
+        public List<ComicModel> Comics { get; set; }
+        public List<VillainModel> Villains { get; set; }
+
+        Task GetComics();
+        Task GetVillains();
+        Task<VillainModel> GetSingleVillain(int? id);
+    }
+}
---
+++ BlazorVillainsApp/Client/Services/VillainServices/VillainService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Json;
+
+namespace BlazorVillainsApp.Client.Services.VillainServices
+{
+    public class VillainService : IVillainService
+    {
+        private readonly HttpClient _http;
+
+        public VillainService(HttpClient http)
+        {
+            _http = http;
+        }
+
+
+
+        public List<ComicModel> Comics { get; set; } = new List<ComicModel>();
+        public List<VillainModel> Villains { get; set; } = new List<VillainModel>();
+
+        public Task GetComics()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VillainModel> GetSingleVillain(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task GetVillains()
+        {
+            var result = await _http.GetFromJsonAsync<List<VillainModel>>("api/villains");
+            if (result != null) Villains = result;
+        }
+    }
+}
