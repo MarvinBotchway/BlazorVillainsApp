a802e283264d721f806a5ea7b15da61b21d6a7d3
Marvin Botchway <mnob.700@gmail.com>
2022-05-30 00:21:01 +0000

    Add DatabaseContext Which Inherits From DBContext
    Added in a new folder called Data with seed data

---
+++ BlazorVillainsApp/Server/Data/DatabaseContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorVillainsApp.Server.Data
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public DatabaseContext( DbContextOptions<DatabaseContext> options ) : base(options)
+        {
+
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ComicModel>().HasData
+            (
+                new ComicModel() { Id = 1, Name = "DC" },
+                new ComicModel() { Id = 2, Name = "Marvel" }
+            );
+
+            modelBuilder.Entity<VillainModel>().HasData
+            (
+
+                new VillainModel()
+                {
+                    Id = 1,
+                    FirstName = "Jack",
+                    LastName = "Napier",
+                    VillainName = "Venom",
+                    ComicId = 2
+                },
+                new VillainModel()
+                {
+                    Id = 2,
+                    FirstName = "Eddie",
+                    LastName = "Brock",
+                    VillainName = "Joker",
+                    ComicId = 1
+                }
+                );
+
+        }
+
+
+
+    }
+}
